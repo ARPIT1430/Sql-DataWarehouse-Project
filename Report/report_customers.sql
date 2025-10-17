a5fc96fe2aad836d87c4aa9a1b87db2bea609cdd
ARPIT1430 <154598852+ARPIT1430@users.noreply.github.com>
2025-10-17 04:58:57 -0700

    Enhance customer report with segmentation logic
    Added a CTE to segregate customers based on age and sales metrics.

--- Report/report_customers.sql
+++ Report/report_customers.sql
@@ -23,3 +23,87 @@ Highlights:
 
 
 
+WITH base_query AS (
+/*---------------------------------------------------------------------------
+1) Base Query: Retrieves core columns from tables
+---------------------------------------------------------------------------*/
+	SELECT 
+	f.order_number , 
+	f.product_key , 
+	f.order_date, 
+	f.sales_amount , 
+	f.quantity,
+	c.customer_key,
+	c.customer_number, 
+	CONCAT(c.first_name , ' ' , c.last_name) AS customer_name,
+	c.birthdate ,
+	DATEDIFF(year,c.birthdate,GETDATE()) AS age 
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_customers c
+	ON f.customer_key = c.customer_key 
+	WHERE f.order_date IS NOT NULL 
+) ,
+	customer_segregations AS (
+	SELECT 
+	customer_name , 
+	customer_number ,
+	customer_key , 
+	age ,
+	COUNT(DISTINCT order_number) AS total_orders ,
+	SUM(sales_amount) AS total_sales ,
+	SUM(quantity) AS total_quantity ,
+	COUNT(DISTINCT product_key) AS total_products ,
+	MAX(order_date) AS last_order_date,
+	DATEDIFF(month,MIN(order_date),MAX(order_date)) AS lifespan 
+	FROM base_query 
+	GROUP BY 
+		customer_name , 
+		customer_number ,
+		customer_key , 
+		age 
+	
+/*
+Grouping by 4 columns means:
+SQL treats the combination (customer_name, customer_number, customer_key, age) as one unique identifier.
+If any of those 4 values differ, they form a new group.
+*/
+	
+)
+SELECT 
+	customer_name , 
+	customer_number ,
+	customer_key ,
+	CASE 
+		WHEN age<20 THEN 'Below 20'
+		WHEN age BETWEEN 20 AND 29 THEN '20-29'
+		WHEN age BETWEEN 30 AND 39 THEN '30-39'
+		WHEN age BETWEEN 40 AND 49 THEN '40-49'
+	END AS age_group ,
+	CASE 
+    WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
+    WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
+    ELSE 'New'
+	END AS customer_segment,
+	DATEDIFF(month,last_order_date,GETDATE()) AS recency ,
+	total_orders, 
+	total_sales ,
+	total_quantity , 
+	total_products ,
+	lifespan ,
+
+	--Compute average order value (AVO)
+	CASE 
+		WHEN total_orders =0 THEN 0 
+		ELSE total_sales/total_orders
+	END AS avg_order_value ,
+
+	-- Compuate average monthly spend
+	CASE 
+		WHEN lifespan =0 THEN 0 
+		ELSE total_sales/lifespan 
+	END AS avg_monthly_spend 
+FROM customer_segregations 
+
+
+
+
