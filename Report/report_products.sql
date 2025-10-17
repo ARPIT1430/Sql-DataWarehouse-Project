21492edf48ff3d7d6aa08e049e07ce5d1fc9e34d
ARPIT1430 <154598852+ARPIT1430@users.noreply.github.com>
2025-10-17 06:30:14 -0700

    Create view for product sales report
    This SQL script creates a view named 'gold.report_products' that aggregates product sales data, including metrics like total orders, total sales, and average selling price. It also categorizes products into segments based on sales performance.

--- Report/report_products.sql
+++ Report/report_products.sql
@@ -20,3 +20,96 @@ Highlights:
        - average monthly revenue
 ===============================================================================
 */
+
+-- =============================================================================
+-- Create Report: gold.report_products
+-- =============================================================================
+
+IF OBJECT_ID ( 'gold.report_products', 'V') IS NOT NULL
+    DROP view gold.report_products ;
+GO
+
+CREATE VIEW gold.report_products AS 
+WITH base_query AS ( 
+/*---------------------------------------------------------------------------
+1) Base Query: Retrieves core columns from fact_sales and dim_products
+---------------------------------------------------------------------------*/
+SELECT 
+    f.order_number, 
+    f.order_date, 
+    f.customer_key, 
+    f.sales_amount, 
+    f.quantity,
+    p.product_key, 
+    p.product_name, 
+    p.category, 
+    p.subcategory,
+    p.product_cost
+FROM gold.fact_sales f 
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key 
+), 
+
+product_aggregations AS (
+/*---------------------------------------------------------------------------
+2) Product Aggregations: Summarizes key metrics at the product level
+---------------------------------------------------------------------------*/
+SELECT 
+    product_key,
+    product_name,  
+    category,  
+    subcategory, 
+    product_cost,
+    COUNT(DISTINCT order_number) AS total_orders,
+    COUNT(DISTINCT customer_key) AS total_customers, 
+    SUM(sales_amount) AS total_sales, 
+    SUM(quantity) AS total_quantity, 
+    MAX(order_date) AS last_sale_date,
+    DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan, 
+    ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
+FROM base_query
+GROUP BY 
+    product_key,
+    product_name,  
+    category,  
+    subcategory, 
+    product_cost
+)
+
+/*---------------------------------------------------------------------------
+3) Final Query: Combines all product results into one output
+---------------------------------------------------------------------------*/
+SELECT 
+    product_key, 
+    product_name, 
+    category, 
+    subcategory,
+    DATEDIFF(month, last_sale_date, GETDATE()) AS recency, 
+    CASE
+        WHEN total_sales > 50000 THEN 'High-Performer'
+        WHEN total_sales >= 10000 THEN 'Mid-Range'
+        ELSE 'Low-Performer'
+    END AS product_segment,
+    lifespan, 
+    total_orders, 
+    total_sales, 
+    total_quantity, 
+    total_customers, 
+    avg_selling_price,
+
+    -- avg_order_revenue (AOR)
+    CASE 
+        WHEN total_orders = 0 THEN 0 
+        ELSE total_sales / total_orders 
+    END AS avg_order_revenue, 
+
+    -- avg_monthly_revenue (AMR)
+    CASE 
+        WHEN lifespan = 0 THEN 0 
+        ELSE total_sales / lifespan 
+    END AS avg_monthly_revenue
+
+FROM product_aggregations
+
+
+SELECT * FROM gold.report_products
